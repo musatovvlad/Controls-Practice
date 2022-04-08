4ddb7476b6864d0a28efdf1272958b60978c4b3e
Musatov Vladimir <vladimir.pmz@gmail.com>
2022-04-08 23:55:23 +0300

    Implemented switches setting and getting

--- Controls Practice/ViewController.swift
+++ Controls Practice/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
-    @IBOutlet var switches: [UISwitch]!
+      @IBOutlet var switches: [UISwitch]!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
-    var number: UInt8 = 128{
+    var number: UInt8 = 128 {
         didSet {
             updateUi()
         }
@@ -21,14 +21,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rotateSwiths()
       updateUi()
+    }
+    /// Rotate switch
+    func rotateSwiths (){
+        for `switch` in switches {
+            `switch`.layer.transform = CATransform3DMakeRotation(-.pi/2, 0, 0, 1)
+        }
+    }
+    /// Update number from the switches set
+    func updateNamberFromSwitches () {
+        var number = 0
+        for `switch` in switches {
+            number += `switch`.isOn ? `switch`.tag : 0
+        }
+        self.number = UInt8(number % 256)
+    }
+    
+    
+    /// Update switches from the number
+    func updateSwitches (){
+        for `switch` in switches {
+            `switch`.isOn = Int(number) & `switch`.tag != 0
+        }
+        
     }
     /// update oll outlets to number
     func updateUi() {
         button.setTitle("\(number)", for: [])
-        
-        // TODO: set sveetches to number
-        
+        updateSwitches ()
         slider.value = Float(number)
         textField.text = "\(number)"
     }
@@ -37,8 +59,10 @@ class ViewController: UIViewController {
        number = UInt8((Int(number) + 1) % 256)
          
     }
+           
+    
     @IBAction func switchToggled(_ sender: UISwitch) {
-        print(#line, #function, sender.tag)
+      updateNamberFromSwitches()
     }
     @IBAction func sliderMoved() {
         number = UInt8(slider.value)
