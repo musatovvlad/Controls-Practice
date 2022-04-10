b2cff5b9a6eaff6b2bdf14abdaed14aa28bfb0e9
Musatov Vladimir <vladimir.pmz@gmail.com>
2022-04-10 12:56:05 +0300

    Added gesture recognizer

--- Controls Practice/ViewController.swift
+++ Controls Practice/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
-      @IBOutlet var switches: [UISwitch]!
+    @IBOutlet var switches: [UISwitch]!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
-    var number: UInt8 = 128 {
+    var number = 128 {
         didSet {
+         number = (number + 256) % 256
             updateUi()
         }
     }
@@ -22,7 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         rotateSwiths()
-      updateUi()
+        updateUi()
     }
     /// Rotate switch
     func rotateSwiths (){
@@ -36,7 +37,7 @@ class ViewController: UIViewController {
         for `switch` in switches {
             number += `switch`.isOn ? `switch`.tag : 0
         }
-        self.number = UInt8(number % 256)
+        self.number = number
     }
     
     
@@ -54,21 +55,29 @@ class ViewController: UIViewController {
         slider.value = Float(number)
         textField.text = "\(number)"
     }
-
+    
     @IBAction func buttonPressed() {
-       number = UInt8((Int(number) + 1) % 256)
-         
+        number += 1
+        
     }
-           
+    
     
     @IBAction func switchToggled(_ sender: UISwitch) {
-      updateNamberFromSwitches()
+        updateNamberFromSwitches()
     }
     @IBAction func sliderMoved() {
-        number = UInt8(slider.value)
+        number = Int(slider.value)
     }
     @IBAction func textFieldEdited() {
-        number = UInt8(textField.text ?? "") ?? 128
+        number = Int(textField.text ?? "") ?? 128
+    }
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        if location.x < view.bounds.midX{
+            number -= 1
+        }else{
+            number += 1
+        }
     }
     
 }
